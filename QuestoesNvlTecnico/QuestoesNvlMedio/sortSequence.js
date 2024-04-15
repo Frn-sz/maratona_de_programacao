59cd0fb4d55e099178461876ed5be48190a00c4c
Luis Felipe Ritta <luis.feliperitta@gmail.com>
2024-04-15 13:00:27 -0300

    Update sortSequence.js

--- QuestoesNvlTecnico/QuestoesNvlMedio/sortSequence.js
+++ QuestoesNvlTecnico/QuestoesNvlMedio/sortSequence.js
@@ -18,23 +18,6 @@ function sortSequence(array) {
         }
     }
 
-    for (let i = 0; i < slices.length; i++) {
-        // ordenando cada divisao
-        let array = slices[i];
-        let len = array.length;
-        for (let i = 0; i < len - 1; i++) {
-            for (let j = 0; j < len - i - 2; j++) {
-                if (array [j] > array [j + 1]) {
-                    temp = array[j + 1];
-                    array[j + 1] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-    }
-
-    temp = tempSum = 0;
-
     // ordenando pedaços ordenados de acordo com as somas
     for (let i = 0; i < sums.length; i++) {
         for (let j = 0; j < sums.length - 1; j++) {
@@ -57,12 +40,12 @@ function sortSequence(array) {
             output.push(each[i2]);
         }
     }
-    
-    // console.log(output);
 
     return output;
 }
 
 sortSequence([3,2,1,0,5,6,4,0,1,5,3,0,4,2,8,0]);
+
 sortSequence([3,2,1,0,5,6,4,0,1,5,3,0,2,2,2,0]);
+
 sortSequence([2,2,2,0,5,6,4,0,1,5,3,0,3,2,1,0]);
